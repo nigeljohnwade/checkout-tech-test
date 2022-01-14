c5d42463cc1c5d9a4d84a563f5426ef69a7d7635
nigeljohnwade <nigeljohnwade@gmail.com>
2022-01-14 10:48:53 +0000

    Test for fails

--- src/checkout.test.js
+++ src/checkout.test.js
@@ -1,46 +1,56 @@
 import { checkout } from './checkout.js';
 
 const sampleBasket1 = [
-    {type: 'standard', price: 790, isThursday: true},
-    {type: 'standard', price: 790, isThursday: true},
-    {type: 'concession', price: 540, isThursday: true},
-    {type: 'concession', price: 540, isThursday: true},
-    {type: 'concession', price: 540, isThursday: true},
-    {type: 'real3D', price: 90},
-    {type: 'real3D', price: 90},
-    {type: 'IMAX', price: 150},
-    {type: 'IMAX', price: 150},
+    {product: 'ticket', type: 'standard', price: 790, isThursday: true},
+    {product: 'ticket', type: 'standard', price: 790, isThursday: true},
+    {product: 'ticket', type: 'concession', price: 540, isThursday: true},
+    {product: 'ticket', type: 'concession', price: 540, isThursday: true},
+    {product: 'ticket', type: 'concession', price: 540, isThursday: true},
+    {product: 'extra', type: 'real3D', price: 90},
+    {product: 'extra', type: 'real3D', price: 90},
+    {product: 'extra', type: 'IMAX', price: 150},
+    {product: 'extra', type: 'IMAX', price: 150},
 ];
 
 
 const sampleBasket2 = [
-    {type: 'standard', price: 790, isThursday: false},
-    {type: 'standard', price: 790, isThursday: false},
-    {type: 'concession', price: 540, isThursday: false},
-    {type: 'concession', price: 540, isThursday: false},
-    {type: 'concession', price: 540, isThursday: false},
-    {type: 'real3D', price: 90},
-    {type: 'real3D', price: 90},
-    {type: 'IMAX', price: 150},
-    {type: 'IMAX', price: 150},
+    {product: 'ticket', type: 'standard', price: 790, isThursday: false},
+    {product: 'ticket', type: 'standard', price: 790, isThursday: false},
+    {product: 'ticket', type: 'concession', price: 540, isThursday: false},
+    {product: 'ticket', type: 'concession', price: 540, isThursday: false},
+    {product: 'ticket', type: 'concession', price: 540, isThursday: false},
+    {product: 'extra', type: 'real3D', price: 90},
+    {product: 'extra', type: 'real3D', price: 90},
+    {product: 'extra', type: 'IMAX', price: 150},
+    {product: 'extra', type: 'IMAX', price: 150},
 ];
 
 const sampleBasket3 = [
-    {type: 'standard', price: 790, isThursday: false},
-    {type: 'standard', price: 790, isThursday: true},
-    {type: 'concession', price: 540, isThursday: false},
-    {type: 'concession', price: 540, isThursday: true},
-    {type: 'concession', price: 540, isThursday: false},
-    {type: 'real3D', price: 90},
-    {type: 'real3D', price: 90},
-    {type: 'IMAX', price: 150},
-    {type: 'IMAX', price: 150},
+    {product: 'ticket', type: 'standard', price: 790, isThursday: false},
+    {product: 'ticket', type: 'standard', price: 790, isThursday: true},
+    {product: 'ticket', type: 'concession', price: 540, isThursday: false},
+    {product: 'ticket', type: 'concession', price: 540, isThursday: true},
+    {product: 'ticket', type: 'concession', price: 540, isThursday: false},
+    {product: 'extra', type: 'real3D', price: 90},
+    {product: 'extra', type: 'real3D', price: 90},
+    {product: 'extra', type: 'IMAX', price: 150},
+    {product: 'extra', type: 'IMAX', price: 150},
 ];
 
 test('Sample basket 1', () => {
     expect(checkout(sampleBasket1)).toMatchObject({
         totalPrice: 2350,
         savings: 1330,
+        isThursdayOfferCount: 5,
+    });
+    expect(checkout(sampleBasket1)).not.toMatchObject({
+        totalPrice: 2350,
+        savings: 1330,
+        isThursdayOfferCount: 2,
+    });
+    expect(checkout(sampleBasket1)).not.toMatchObject({
+        totalPrice: 3680,
+        savings: 0,
         isThursdayOfferCount: 5,
     });
 });
@@ -51,6 +61,16 @@ test('Sample basket 2', () => {
         savings: 0,
         isThursdayOfferCount: 0,
     });
+    expect(checkout(sampleBasket2)).not.toMatchObject({
+        totalPrice: 3680,
+        savings: 0,
+        isThursdayOfferCount: 5,
+    });
+    expect(checkout(sampleBasket2)).not.toMatchObject({
+        totalPrice: 2350,
+        savings: 1330,
+        isThursdayOfferCount: 5,
+    });
 });
 
 test('Sample basket 3', () => {
@@ -58,5 +78,15 @@ test('Sample basket 3', () => {
         totalPrice: 3140,
         savings: 540,
         isThursdayOfferCount: 2,
+    });
+    expect(checkout(sampleBasket3)).not.toMatchObject({
+        totalPrice: 3680,
+        savings: 0,
+        isThursdayOfferCount: 2,
+    });
+    expect(checkout(sampleBasket3)).not.toMatchObject({
+        totalPrice: 1480,
+        savings: 540,
+        isThursdayOfferCount: 0,
     });
 });
